32f2b1b7415d2bb07cf80ec95b5b5de3b115e72d
Serhii Tarasenko <stdev333@gmail.com>
2024-05-16 20:16:53 +0300

    add User model

---
+++ models/User.js
@@ -0,0 +1,38 @@
+import { Schema, model } from "mongoose";
+import { handleSaveError, setUpdateSettings } from "./hooks.js";
+import { emailRegexp } from "../constants/constants.js";
+
+const userSchema = new Schema(
+  {
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      match: emailRegexp,
+    },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter",
+    },
+    token: {
+      type: String,
+      default: null,
+    },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+userSchema.post("save", handleSaveError);
+
+userSchema.pre("findOneAndUpdate", setUpdateSettings);
+
+userSchema.post("findOneAndUpdate", handleSaveError);
+
+const User = model("user", userSchema);
+
+export default User;
